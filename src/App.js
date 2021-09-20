e27b91ee9db8b497239b2fcd7e139014e26392a1
Valentyn Shlapachenko <vshlapac@sfu.ca>
2021-09-19 21:54:09 -0700

    Initial commit of React course. Started React App. Created controlled elements. Went through css modules props, jsx, useState(), useRef(), functional/class components, event prevent default, binding and reactdevtools

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react'
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/ui/button/MyButton";
+import MyInput from "./components/ui/input/MyInput";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},
+    {id: 4, title: 'Javascript 4', body: 'Description'}
+  ])
+
+  const [post, setPost] = useState({title: '', body: ''})
+  const [body, setBody] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
+          type="text"
+          placeholder="Post Name"
+        />
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
+          type="text"
+          placeholder="Post Description"
+        />
+        <MyButton onClick={addNewPost}>Create Post</MyButton>
+      </form>
+      <PostList posts={posts} title='Post List About JavaScript'/>
+
     </div>
   );
 }
